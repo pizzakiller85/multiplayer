1d02b1e58dd8e066895911159115a464aa6f7f09
Dan <danielchapko@gmail.com>
2024-07-08 22:31:25 -0700

    a star is born, first commit

---
+++ wwwroot/js/game.js
@@ -0,0 +1,46 @@
+﻿const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+let max = 200;
+let x = canvas.width / 2 + Math.floor(Math.random() * max);
+let y = canvas.height / 2 + Math.floor(Math.random() * max);
+const size = 20;
+const speed = 10;
+
+
+function drawAll(allPostions) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    allPostions.forEach(function (val, key, map) {
+        ctx.fillStyle = '#'+ val.Key;//rgb(255, 200, 0);
+        
+        var x = val.Value.x;
+        var y = val.Value.y;
+        
+        ctx.fillRect(x, y, size, size);
+    });
+}
+
+function drawSquare(color) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, size, size);
+}
+
+function moveSquare(e) {
+    switch (e.key) {
+        case 'ArrowUp':
+            y -= speed;
+            break;
+        case 'ArrowDown':
+            y += speed;
+            break;
+        case 'ArrowLeft':
+            x -= speed;
+            break;
+        case 'ArrowRight':
+            x += speed;
+            break;
+    }
+}
+
+document.addEventListener('keydown', moveSquare);
+drawSquare();
