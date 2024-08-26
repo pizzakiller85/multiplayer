b2739d9dfb8517096dc6afdf40d25778c2153a82
Dan <danielchapko@gmail.com>
2024-08-25 22:04:43 -0700

    fix chathub

--- wwwroot/js/game.js
+++ wwwroot/js/game.js
@@ -1,35 +1,5 @@
 ﻿"use strict";
 var shareStateTimeInterval = 10;
-var userid = createHEXColor();
-function createHEXColor() {
-    let color = Math.floor(Math.random() * 16777215).toString(16);
-    return color.padStart(6, '0');
-}
-function getRandomHexColor() {
-    let color = Math.floor(Math.random() * 16777215).toString(16);
-    return '#' + color.padStart(6, '0');
-}
-
-var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
-connection.on("AllUserPositions", function (positions) {
-    var allPositions = JSON.parse(positions);
-    drawAll(allPositions);
-});
-
-connection.start().then(function () {
-    console.log("start interval");
-    window.setInterval(shareState, shareStateTimeInterval);
-}).catch(function (err) {
-    return console.error(err.toString());
-});
-
-function shareState() {
-    movePlayer();
-    connection.invoke("ShareUserPosition", userid.toString(), x, y).catch(function (err) {
-        return console.error(err.toString());
-    });
-}
-
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 let max = 200;
@@ -44,6 +14,41 @@ var acc = 0.12;
 var friction = 0.98;
 
 let keysPressed = {};
+
+
+var userid = createHEXColor();
+function createHEXColor() {
+    let color = Math.floor(Math.random() * 16777215).toString(16);
+    return color.padStart(6, '0');
+}
+function getRandomHexColor() {
+    let color = Math.floor(Math.random() * 16777215).toString(16);
+    return '#' + color.padStart(6, '0');
+}
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+// step 2 get all positions
+connection.on("AllUserPositions", function (positions) {
+    var allPositions = JSON.parse(positions);
+    drawAll(allPositions);
+});
+
+connection.start().then(function () {
+    console.log("start interval");
+    window.setInterval(shareState, shareStateTimeInterval); // on signalr load step 0, set interval
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+
+// main loop --  step 1
+function shareState() {
+    movePlayer();
+    connection.invoke("ShareUserPosition", userid.toString(), x, y).catch(function (err) {
+        return console.error(err.toString());
+    });
+}
 
 function drawAll(allPostions) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
