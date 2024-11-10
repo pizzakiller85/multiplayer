171a1f1f9004f716d618867fcd460116a13252b5
Dan <danielchapko@gmail.com>
2024-11-10 15:27:27 -0800

    fix

--- wwwroot/js/game.js
+++ wwwroot/js/game.js
@@ -112,6 +112,7 @@ function drawAll(allPostions) {
     document.getElementById("playerCount").innerHTML = count;
 }
 
+
 function distance(x1, y1, x2, y2) {
     return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 }
